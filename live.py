06d79776f6f67368d3900c5fdd28a1c03c2f9165
nailavann <nailavann@hotmail.com>
2022-06-21 21:52:09 +0300

    .

--- live.py
+++ live.py
@@ -2,34 +2,23 @@ import numpy as np
 import cv2
 from keras.models import load_model
 from keras.preprocessing.image import img_to_array
-#proje : evrişimsel sinir ağları kullanarak yüz analizinden duygu durumu, cinsiyet , etnik köken, yaş aralığı tespiti.
-#duygu durumu ayrı bir veri seti
-#yaş cinsiyet yaş aralığı ayrı bir veri seti
 
-
-#duygu durumu 7 tane sınıfdan oluşuyor.
 emotion_labels = ['Sinirli','Igrenme','Korku','Mutlu','Uzgun', 'Saskin', 'Dogal']
 
-#etnik köken 5 sınıftan oluşuyor
 etnik_labels = ['Beyaz',"Siyah","Asyalı",'Hintli',"Diğer"]
 
-#yaş aralığı 3 sınıftan oluşuyor.0 ile 24 yaş arasını gen.24 55 yaş arasını orta , 55 yaş üstüne yaşlu olarak tanımlıyoruz.
 age_labels = ['Orta','Yasli','Genc']
 
-#cinsiyet 2 sınıftan oluşuyor.
 gender_labels = ['Erkek','Kadin']
 
-#2 ayrı veri setinden 4 ayrı model oluşturuldu.e
 emotion_classifier =load_model('emotion_model.h5')
 gender_classifier =load_model('gender_model.h5')
 etnik_classifier = load_model('etnik_model.h5')
 age_classifier = load_model('age_model.h5')
 
-#yüzü tespit etmek için haar cascade sınıflandırıcısı kullanıldı
 face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 
-#kamera açıcaksak 0 deriz.
 cap = cv2.VideoCapture(0)
 
 
@@ -39,13 +28,10 @@ while cap.isOpened():
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
 
-    #yüzü tespit ediyoruz.webcam için
     faces = face_detector.detectMultiScale(gray,1.2,7)
 
-    #video için
-    #faces = face_detector.detectMultiScale(gray,1.3,8)
+   
 
-    #x,y,w,h değerlerine yüzün koordinatlarını atıyoruz.
     for (x,y,w,h) in faces:
         
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
@@ -57,7 +43,6 @@ while cap.isOpened():
         roi = img_to_array(roi)   
 
 
-        #satır sütun haline gelir.matris
         roi = np.expand_dims(roi,axis=0)
            
         
@@ -69,9 +54,6 @@ while cap.isOpened():
 
 
        
-
-        #prediction olasılıkları döndürüyor.olasılıkları toplamı 1
-        #dizideki en büyük olasılığı alıyoruz
         emotion=emotion_labels[prediction.argmax()]
         etnik = etnik_labels[etnik_pred.argmax()]
         age = age_labels[age_pred.argmax()]
